c65b05fba15dd717821f1b0e1bf79dfe660b8e7a
DimasAdiSaputro <dimasadisaputr0699@gmail.com>
2025-08-18 06:35:00 +0700

    modify role

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('/')
+            ->authGuard('web')
             ->darkMode(true)
             ->favicon(asset('logo.jpg'))
             ->login(\App\Filament\Admin\Pages\Auth\Login::class)
